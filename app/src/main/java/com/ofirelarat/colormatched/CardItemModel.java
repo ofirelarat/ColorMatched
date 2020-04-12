3c75e6c400d55a39d705abe4f434b739271ec069
ofirelarat <96ofir11@gmail.com>
2020-04-12 15:46:06 +0300

    add timer to  game

--- app/src/main/java/com/ofirelarat/colormatched/CardItemModel.java
+++ app/src/main/java/com/ofirelarat/colormatched/CardItemModel.java
@@ -1,9 +1,11 @@
 package com.ofirelarat.colormatched;
 
 import android.content.Context;
+import android.view.contentcapture.ContentCaptureCondition;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class CardItemModel {
     public static final Map<Integer,Integer> colorNameToValueIds = getColorsNamesToValueMap();
@@ -13,6 +15,8 @@ public class CardItemModel {
     private int meaningColorViewColor;
     private int matchingColorViewColor;
     private boolean isAMatch;
+
+    public CardItemModel(){ }
 
     public CardItemModel(Context context, int meaningColorName, int matchingColorName, int meaningColorViewColor, int matchingColorViewColor, boolean isAMatch) {
         this.meaningColorName = context.getResources().getString(meaningColorName);
@@ -30,32 +34,32 @@ public class CardItemModel {
         return meaningColorName;
     }
 
-    public void setMeaningColorName(String meaningColorName) {
-        this.meaningColorName = meaningColorName;
+    public void setMeaningColorName(Context context, int meaningColorName) {
+        this.meaningColorName = context.getResources().getString(meaningColorName);
     }
 
     public String getMatchingColorName() {
         return matchingColorName;
     }
 
-    public void setMatchingColorName(String matchingColorName) {
-        this.matchingColorName = matchingColorName;
+    public void setMatchingColorName(Context context, int matchingColorName) {
+        this.matchingColorName = context.getResources().getString(matchingColorName);
     }
 
     public int getMeaningColorViewColor() {
         return meaningColorViewColor;
     }
 
-    public void setMeaningColorViewColor(int meaningColorViewColor) {
-        this.meaningColorViewColor = meaningColorViewColor;
+    public void setMeaningColorViewColor(Context context, int meaningColorViewColor) {
+        this.meaningColorViewColor = context.getResources().getColor(meaningColorViewColor);
     }
 
     public int getMatchingColorViewColor() {
         return matchingColorViewColor;
     }
 
-    public void setMatchingColorViewColor(int matchingColorViewColor) {
-        this.matchingColorViewColor = matchingColorViewColor;
+    public void setMatchingColorViewColor(Context context, int matchingColorViewColor) {
+        this.matchingColorViewColor = context.getResources().getColor(matchingColorViewColor);
     }
 
     public boolean isAMatch() {
@@ -64,6 +68,49 @@ public class CardItemModel {
 
     public void setAMatch(boolean AMatch) {
         isAMatch = AMatch;
+    }
+
+    public static CardItemModel getRandomizeCard(Context context){
+        CardItemModel cardItemModel = new CardItemModel();
+
+        Random random =new Random();
+        final int colorsCount = colorNameToValueIds.size();
+
+        int randomColor = random.nextInt(colorsCount);
+        int randomColorRes = getRandomColorRes(randomColor);
+        cardItemModel.setMeaningColorName(context, randomColorRes);
+        cardItemModel.setMeaningColorViewColor(context, colorNameToValueIds.get(randomColorRes));
+
+        boolean isAMatch = random.nextBoolean();
+        cardItemModel.setAMatch(isAMatch);
+        if(isAMatch){
+            cardItemModel.setMatchingColorName(context, randomColorRes);
+            cardItemModel.setMatchingColorViewColor(context, colorNameToValueIds.get(randomColorRes));
+        }else {
+            int randomMatchColor = random.nextInt(colorsCount);
+            int randomMatchColorRes = getRandomColorRes(randomMatchColor);
+            cardItemModel.setMatchingColorName(context, randomMatchColorRes);
+            cardItemModel.setMatchingColorViewColor(context, colorNameToValueIds.get(randomMatchColorRes));
+        }
+
+        return cardItemModel;
+    }
+
+    private static int getRandomColorRes(int randomInx){
+        switch (randomInx){
+            case 0:
+                return R.string.black;
+            case 1:
+                return R.string.blue;
+            case 2:
+                return R.string.green;
+            case 3:
+                return R.string.orange;
+            case 4:
+                return R.string.red;
+            default:
+                return R.string.black;
+        }
     }
 
     private static Map<Integer, Integer> getColorsNamesToValueMap(){
--- app/src/main/java/com/ofirelarat/colormatched/MainActivity.java
+++ app/src/main/java/com/ofirelarat/colormatched/MainActivity.java
@@ -1,50 +1,54 @@
 package com.ofirelarat.colormatched;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
 import android.view.animation.LinearInterpolator;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.yuyakaido.android.cardstackview.CardStackLayoutManager;
 import com.yuyakaido.android.cardstackview.CardStackListener;
 import com.yuyakaido.android.cardstackview.CardStackView;
 import com.yuyakaido.android.cardstackview.Direction;
+import com.yuyakaido.android.cardstackview.Duration;
 import com.yuyakaido.android.cardstackview.StackFrom;
+import com.yuyakaido.android.cardstackview.SwipeAnimationSetting;
 import com.yuyakaido.android.cardstackview.SwipeableMethod;
+
+import ticker.views.com.ticker.widgets.circular.timer.callbacks.CircularViewCallback;
+import ticker.views.com.ticker.widgets.circular.timer.view.CircularView;
 
 public class MainActivity extends AppCompatActivity implements CardStackListener {
 
     private RecyclerView.Adapter cardsAdapter;
     private CardStackLayoutManager manager;
+    private CardStackView cardStackView;
+
+    private TextView scoreText;
+    private CircularView progressBar;
+
     private CardItemModel[] colorsItems;
+    private int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        cardStackView = (CardStackView)findViewById(R.id.card_stack_view);
+        scoreText = findViewById(R.id.scoreText);
+        progressBar = findViewById(R.id.circular_view);
+
         colorsItems = getColorsItems();
-
-        cardsAdapter = new CardItemAdapter(colorsItems);
-        manager = new CardStackLayoutManager(this, this);
-        manager.setVisibleCount(3);
-        manager.setStackFrom(StackFrom.Top);
-        manager.setTranslationInterval(8f);
-        manager.setScaleInterval(0.90f);
-        manager.setMaxDegree(20f);
-        manager.setSwipeThreshold(0.3f);
-        manager.setSwipeableMethod(SwipeableMethod.AutomaticAndManual);
-        manager.setOverlayInterpolator(new LinearInterpolator());
-        manager.setDirections(Direction.HORIZONTAL);
-
-
-        CardStackView cardStackView = (CardStackView)findViewById(R.id.card_stack_view);
-        cardStackView.setLayoutManager(manager);
-        cardStackView.setAdapter(cardsAdapter);
+        initCardStackView();
+        initTimerView();
     }
 
     @Override
@@ -59,6 +63,8 @@ public class MainActivity extends AppCompatActivity implements CardStackListener
         if((swipedCard.isAMatch() && direction == Direction.Right)
                 || ((!swipedCard.isAMatch()) && direction == Direction.Left)){
             Toast.makeText(MainActivity.this, "correct!", Toast.LENGTH_LONG).show();
+            score += 100;
+            scoreText.setText(String.valueOf(score));
         }else{
             Toast.makeText(MainActivity.this, "error!", Toast.LENGTH_LONG).show();
         }
@@ -84,15 +90,101 @@ public class MainActivity extends AppCompatActivity implements CardStackListener
 
     }
 
+    public void onClickIncorrectBtn(View view) {
+        SwipeAnimationSetting setting = new SwipeAnimationSetting.Builder()
+                .setDirection(Direction.Left)
+                .setDuration(Duration.Slow.duration)
+                .setInterpolator(new AccelerateInterpolator())
+                .build();
+        manager.setSwipeAnimationSetting(setting);
+        cardStackView.swipe();
+    }
+
+    public void onClickCorrectBtn(View view) {
+        SwipeAnimationSetting setting = new SwipeAnimationSetting.Builder()
+                .setDirection(Direction.Right)
+                .setDuration(Duration.Slow.duration)
+                .setInterpolator(new AccelerateInterpolator())
+                .build();
+        manager.setSwipeAnimationSetting(setting);
+        cardStackView.swipe();
+    }
+
+    private void initCardStackView(){
+        cardsAdapter = new CardItemAdapter(colorsItems);
+
+        manager = new CardStackLayoutManager(this, this);
+        manager.setVisibleCount(3);
+        manager.setStackFrom(StackFrom.Top);
+        manager.setTranslationInterval(8f);
+        manager.setScaleInterval(0.90f);
+        manager.setMaxDegree(20f);
+        manager.setSwipeThreshold(0.3f);
+        manager.setSwipeableMethod(SwipeableMethod.AutomaticAndManual);
+        manager.setOverlayInterpolator(new LinearInterpolator());
+        manager.setDirections(Direction.HORIZONTAL);
+
+        cardStackView.setLayoutManager(manager);
+        cardStackView.setAdapter(cardsAdapter);
+    }
+
+    private void initTimerView() {
+        CircularView.OptionsBuilder builderWithTimer =
+                new CircularView.OptionsBuilder()
+                        .shouldDisplayText(true)
+                        .setCounterInSeconds(60)
+                        .setCircularViewCallback(new CircularViewCallback() {
+                            @Override
+                            public void onTimerFinish() {
+                                // Will be called if times up of countdown timer
+                                Toast.makeText(MainActivity.this, "Timer Finished ", Toast.LENGTH_SHORT).show();
+                                displayEndGameDialog();
+                            }
+
+                            @Override
+                            public void onTimerCancelled() {
+                                // Will be called if stopTimer is called
+                                Toast.makeText(MainActivity.this, "CircularCallback: Timer Cancelled ", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+        progressBar.setOptions(builderWithTimer);
+        progressBar.startTimer();
+    }
+
     private CardItemModel[] getColorsItems(){
-        CardItemModel[] cardItemModels = new CardItemModel[6];
-        cardItemModels[0] = new CardItemModel(this,R.string.black,R.string.blue,R.color.black,R.color.blue,false);
-        cardItemModels[1] = new CardItemModel(this,R.string.blue,R.string.blue,R.color.blue,R.color.blue,true);
-        cardItemModels[2] = new CardItemModel(this,R.string.green,R.string.green,R.color.black,R.color.green,true);
-        cardItemModels[3] = new CardItemModel(this,R.string.blue,R.string.orange,R.color.red,R.color.orange,false);
-        cardItemModels[4] = new CardItemModel(this,R.string.red,R.string.red,R.color.black,R.color.red,true);
-        cardItemModels[5] = new CardItemModel(this,R.string.orange,R.string.orange,R.color.orange,R.color.orange,true);
+        CardItemModel[] cardItemModels = new CardItemModel[50];
+        for (int i = 0; i<cardItemModels.length; i++) {
+            cardItemModels[i] = CardItemModel.getRandomizeCard(this);
+        }
 
         return cardItemModels;
+    }
+
+    private void displayEndGameDialog(){
+        new AlertDialog.Builder(MainActivity.this)
+                .setTitle("Game Over")
+                .setMessage("your score is: " + score + ", would you like to play again?")
+                .setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        resetGame();
+                    }
+                })
+                .setNegativeButton("no", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .show();
+    }
+
+    private void resetGame(){
+        score = 0;
+        scoreText.setText(String.valueOf(score));
+        colorsItems = getColorsItems();
+        cardStackView.setAdapter(cardsAdapter);
+        initTimerView();
     }
 }
